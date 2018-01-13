e2e1df92883baf4e73b49153615373139728c8b9
ZHYUUUUU <302584840@qq.com>
2018-01-13 16:05:00 +0800

    ActivityTest

---
+++ app/src/main/java/com/example/administrator/activitytest/FirstActivity.java
@@ -0,0 +1,53 @@
+package com.example.administrator.activitytest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+
+public class FirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(FirstActivity.this,"You click Button 1",
+                //Toast.LENGTH_SHORT).show();
+                //finish();
+                Intent intent =new Intent(FirstActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+}
